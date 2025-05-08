9e3b0191ba7d41f580494f560c2f7666a10e895d
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-05-08 23:56:32 +0700

    feat(upload): update component wrapper file upload

---
+++ src/components/wrapper-file-upload/idnex.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
+
+interface TProps {
+  children: React.ReactNode
+  upLoadFunc: (file: File) => void
+  objectAcceptFile?: Record<string, string[]>
+}
+
+const WrapperFileUpload = (props: TProps) => {
+  const { children, upLoadFunc, objectAcceptFile } = props
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: objectAcceptFile ? objectAcceptFile : {},
+    onDrop: acceptedFiles => {
+      upLoadFunc(acceptedFiles[0])
+      console.log(acceptedFiles)
+    }
+  })
+
+  return (
+    <div {...getRootProps({ className: 'dropzone' })}>
+      <input {...getInputProps()} />
+      {children}
+    </div>
+  )
+}
+
+export default WrapperFileUpload
