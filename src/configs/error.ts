fd460099282d5892459f76d6b2207106a00823ce
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-06-13 00:47:00 +0700

    feat(product): done CRUD manage-product, add custom-editor(react-draft-wysiwyg, custom-date-picker and utils formatNumberToLocal, convertHTMLToDraft, formatNumber

--- src/configs/error.ts
+++ src/configs/error.ts
@@ -27,3 +27,8 @@ export const OBJECT_TYPE_ERROR_PRODUCT_TYPE: Record<string, string> = {
   ALREADY_EXIST: 'Data_existed_in_system',
   INTERNAL_SERVER_ERROR: 'Server_is_error'
 }
+
+export const OBJECT_TYPE_ERROR_PRODUCT: Record<string, string> = {
+  ALREADY_EXIST: 'Data_existed_in_system',
+  INTERNAL_SERVER_ERROR: 'Server_is_error'
+}
