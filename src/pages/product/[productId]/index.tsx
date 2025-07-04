f63cd3ab08f5a7456bac90b5fb6fe97f854f0f98
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-07-04 13:54:57 +0700

    feat: ssr with details product

--- src/pages/product/[productId]/index.tsx
+++ src/pages/product/[productId]/index.tsx
@@ -1,15 +1,20 @@
 // ** Import Next
 import { NextPage } from 'next'
 import { ReactNode } from 'react'
+import { getDetailsProductPublicBySlug, getListRelatedProductBySlug } from 'src/services/product'
+import { TProduct } from 'src/types/product'
 
 // * views
 import LayoutNotApp from 'src/views/layouts/LayoutNotApp'
 import DetailsProductPage from 'src/views/pages/product/DetailsProduct'
 
-type TProps = {}
+type TProps = {
+  productData: TProduct
+  listRelatedProduct: TProduct[]
+}
 
-const Index: NextPage<TProps> = () => {
-  return <DetailsProductPage />
+const Index: NextPage<TProps> = ({ productData, listRelatedProduct }) => {
+  return <DetailsProductPage productData={productData} productsRelated={listRelatedProduct} />
 }
 
 export default Index
@@ -18,3 +23,37 @@ Index.getLayout = (page: ReactNode) => <LayoutNotApp>{page}</LayoutNotApp>
 
 Index.guestGuard = false
 Index.authGuard = false
+
+export async function getServerSideProps(context: any) {
+  try {
+    const slugId = context.query?.productId
+
+    const [res, resRelated] = await Promise.all([
+      getDetailsProductPublicBySlug(slugId, true),
+      getListRelatedProductBySlug({ params: { slug: slugId } })
+    ])
+
+    const productData = res?.data
+    const listRelatedProduct = resRelated?.data?.products
+
+    if (!productData?._id) {
+      return {
+        notFound: true
+      }
+    }
+
+    return {
+      props: {
+        productData: productData,
+        listRelatedProduct
+      }
+    }
+  } catch (error) {
+    return {
+      props: {
+        productData: {},
+        listRelatedProduct: []
+      }
+    }
+  }
+}
